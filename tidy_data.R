64677a06b9d1e51059ad05c7a9b723303c5b915b
MATT JOHNSON <johnson.matt1818@gmail.com>
2021-04-30 14:34:08 -0400

    Added SC and FC matrix cleaners

--- tidy_data.R
+++ tidy_data.R
@@ -41,3 +41,77 @@ SC.PCA.clean <- left_join(SC.PCA.clean.temp, Traits.clean, by = "Subject")
 
 write.csv(FC.PCA.clean, "derived_data/FC.PCA.clean.csv")
 write.csv(SC.PCA.clean, "derived_data/SC.PCA.clean.csv")
+
+print("TNPCA Data Cleaned")
+#ok now we will work on FC and SC
+
+#start with FC first, FC and SC are very different
+
+#we can easily extract the subject ids and vectorize it
+FC.Subjects.list <- c(FC$subj.list)
+
+FC.MAT <- matrix(0, nrow = length(FC.Subjects.list), ncol = sum(1:67))
+#then try to populate the matrix with the upper triangle
+for (i in 1:length(FC.Subjects.list)) {
+  mat = matrix(unlist(FC$hcp.cortical.fc[[i]]), ncol = 68)
+  #found this issue where some of the hcp.cortical.fc matrices are empty
+  if (nrow(mat) == 0) {
+    FC.MAT[i, ] = rep(0, sum(1:67))
+  } else {
+    #now we don't want the diag entries
+    mat[lower.tri(mat, diag = T)] = NA
+    upp.tri = mat[!is.na(mat)]
+    FC.MAT[i, ] = upp.tri
+  }
+}
+
+FC.DF <- as.data.frame(FC.MAT)
+
+FC.DF$Subject <- FC.Subjects.list
+
+#clean up a bit
+FC.clean.temp <- FC.DF %>% 
+  select(Subject, everything())
+
+#finally join with traits
+FC.clean <- left_join(FC.clean.temp, Traits.clean, by = "Subject")
+
+#now similar thing for SC
+
+#first we need the subjects ids
+SC.Subjects.list <- c(SC$all.id)
+
+#SC is in a different form, but we should be able to follow a very similar process
+#here is how we extract one 68x68 matrix SC$hcp.sc.count[, , 1]
+#I don't think the dig entries are needed here since they are all 0
+
+SC.MAT <- matrix(0, nrow = length(SC.Subjects.list), ncol = sum(1:67))
+for (i in 1:length(SC.Subjects.list)) {
+  mat = matrix(SC$hcp.sc.count[, , i], ncol = 68)
+  #fix missing data issue
+  if(nrow(mat) == 0) {
+    SC.MAT[i, ] = rep(NA, sum(1:67))
+  } else {
+    #no need for diag
+    mat[lower.tri(mat, diag = T)] = NA 
+    upp.tri = mat[!is.na(mat)]
+    SC.MAT[i, ] = upp.tri
+  }
+}
+SC.DF <- as.data.frame(SC.MAT)
+
+SC.DF$Subject <- SC.Subjects.list
+
+#clean up a bit
+SC.clean.temp <- SC.DF %>% 
+  select(Subject, everything())
+
+#finally join with traits
+SC.clean <- left_join(SC.clean.temp, Traits.clean, by = "Subject")
+
+#now save the clean dataframes
+
+write.csv(FC.clean, "derived_data/FC.clean.csv")
+write.csv(SC.clean, "derived_data/SC.clean.csv")
+
+print("SC and FC Matrix Data Cleaned")
